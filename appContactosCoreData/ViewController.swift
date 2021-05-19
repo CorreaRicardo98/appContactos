93d9b58c605f0901fef243ad7497f814e42a29f3
Mac19 <mac19@iMac-de-Mac.local>
2021-05-19 16:05:45 -0500

    aplicacion hasta guardar, leer y eliminar

--- appContactosCoreData/ViewController.swift
+++ appContactosCoreData/ViewController.swift
@@ -6,18 +6,26 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
-    var contactos = [MiContacto]()
-    var contact:MiContacto?
-    var prueba = MiContacto(nombre: "Luis", telefono: 4433360818, direccion: "hidalgo s/n")
+    var contactos = [Contacto]()
+    var index:Int?
+    
+    // Conexión al contexto de BD
+    let contexto = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var contact:Contacto?
+
 
 
     @IBOutlet weak var tablaContactos: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        contactos.append(prueba)
+        
+        //Leer Datso de COreData
+        cargarCoreData()
+        tablaContactos.reloadData()
         tablaContactos.register(UINib(nibName: "ContactTableViewCell", bundle: nil), forCellReuseIdentifier: "celda")
         tablaContactos.delegate = self
         tablaContactos.dataSource = self
@@ -27,14 +35,22 @@ class ViewController: UIViewController {
     @IBAction func addContact(_ sender: Any) {
         let alerta = UIAlertController(title: "Agregar Contacto", message: "Datos del contacto", preferredStyle: .alert)
         let accionAceptar = UIAlertAction(title: "Añadir", style: .default) { (_) in
-            print("se agrego contacto")
+            // Crear objeto contacto
+            
             guard let nombreText = alerta.textFields?.first?.text else{return}
             guard let telefonoNum = Int64(alerta.textFields?[1].text ?? "0000000000") else{return}
             guard let direccion = alerta.textFields?[2].text else{return}
+            let imagenTemp = UIImageView(image: #imageLiteral(resourceName: "llamada.png"))
             
-            let nuevo = MiContacto(nombre: nombreText, telefono: telefonoNum, direccion: direccion)
+            let nuevo = Contacto(context:self.contexto)
+            nuevo.nombre = nombreText
+            nuevo.telefono = telefonoNum
+            nuevo.direccion = direccion
+            nuevo.imagen = imagenTemp.image!.pngData()
 
             self.contactos.append(nuevo)
+            
+            self.guardarContacto()
             
             self.tablaContactos.reloadData()
             
@@ -50,6 +66,7 @@ class ViewController: UIViewController {
         
         alerta.addTextField { (telefono) in
             telefono.placeholder = "Telefono"
+            telefono.keyboardType = .numberPad
         }
         
         alerta.addTextField { (direccion) in
@@ -57,6 +74,25 @@ class ViewController: UIViewController {
         }
         
         present(alerta, animated: true)
+    }
+    
+    func guardarContacto(){
+        do {
+            try contexto.save()
+            print("correcto")
+        } catch let error as NSError {
+            print("Error al guardar: \(error.localizedDescription)")
+        }
+    }
+    
+    func cargarCoreData(){
+        let fetchRequest: NSFetchRequest <Contacto> = Contacto.fetchRequest()
+        
+        do {
+            contactos = try contexto.fetch(fetchRequest)
+        } catch  {
+            print("error al cargar contactos del coreData: \(error.localizedDescription)")
+        }
     }
 }
 
@@ -69,19 +105,55 @@ extension ViewController:UITableViewDelegate,UITableViewDataSource{
         tablaContactos.deselectRow(at: indexPath, animated: true)
         self.contact = contactos[indexPath.row]
         performSegue(withIdentifier: "editarViewController", sender: self)
+        self.index = indexPath.row
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "editarViewController" {
             let objController = segue.destination as! detalleViewController
             objController.recibirContacto = contact
+            objController.index = self.index
         }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let accionDelete = UIContextualAction(style: .normal, title: "") { (_,_,_) in
+            print("borrar")
+            
+            self.contexto.delete(self.contactos[indexPath.row])
+            self.contactos.remove(at:indexPath.row)
+            
+            self.guardarContacto()
+            self.tablaContactos.reloadData()
+        
+        }
+        accionDelete.image = UIImage(named: "borrar.png")
+        
+        return UISwipeActionsConfiguration(actions: [accionDelete])
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let accionCall = UIContextualAction(style: .normal, title: "") { (_,_,_) in
+            print("llamada")
+        
+        }
+        accionCall.image = UIImage(named: "llamada.png")
+        
+        let accionSmS = UIContextualAction(style: .normal, title: "") { (_,_,_) in
+            print("mensaje")
+        
+        }
+        accionSmS.image = UIImage(named: "mensaje.png")
+        
+        
+        return UISwipeActionsConfiguration(actions: [accionCall,accionSmS])
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tablaContactos.dequeueReusableCell(withIdentifier: "celda", for:indexPath) as! ContactTableViewCell
         cell.nombreContacto.text = contactos[indexPath.row].nombre
-        cell.telefonoContacto.text = "📞 \(contactos[indexPath.row].telefono!)"
+        cell.telefonoContacto.text = "📞 \(contactos[indexPath.row].telefono)"
         
         return cell
     }
--- appContactosCoreData/detalleViewController.swift
+++ appContactosCoreData/detalleViewController.swift
@@ -6,34 +6,99 @@
 //
 
 import UIKit
+import CoreData
 
 class detalleViewController: UIViewController {
-    var recibirContacto:MiContacto?
+    var recibirContacto:Contacto?
+    var index:Int?
+    
+    var contactos = [Contacto]()
+    
+    // Conexión al contexto de BD
+    let contexto = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var contact:Contacto?
+    
 
+    @IBOutlet weak var direccionTextField: UITextField!
+    @IBOutlet weak var telefonoTextField: UITextField!
     @IBOutlet weak var nombreTextField: UITextField!
+    @IBOutlet weak var imagenPerson: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        nombreTextField.text = recibirContacto?.nombre
+        cargarCoreData()
+        //MARK:-gestura de imagenes
+        let gesturas = UITapGestureRecognizer(target: self, action: #selector(clickImagen))
+        gesturas.numberOfTapsRequired = 1
+        gesturas.numberOfTouchesRequired = 1
+        
+        imagenPerson.addGestureRecognizer(gesturas)
+        imagenPerson.isUserInteractionEnabled = true
+        
+        self.telefonoTextField.text = "\(recibirContacto!.telefono)"
+        self.nombreTextField.text = recibirContacto?.nombre
+        self.direccionTextField.text = recibirContacto?.direccion
+    }
+    
+    @objc func clickImagen(gestura: UITapGestureRecognizer){
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true, completion: nil)
     }
     
 
     @IBAction func takePhoto(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .savedPhotosAlbum
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true, completion: nil)
         
     }
     @IBAction func saveButton(_ sender: Any) {
+        
+        do {
+           // contactos[index!].setValue(nombreTextField.text,forkey:"nombre")
+           // contactos[index!].setValue(Int64(telefonoTextField.text!),forkey:"telefono")
+           // contactos[index!].setValue(direccionTextField.text,forkey:"direccion")
+           // contactos[index!].setValue(imagenPerson.image?.pngData(),forkey:"imagen")
+            
+
+            try self.contexto.save()
+            print("guardado correctamente")
+        } catch  {
+            print("error: \(error.localizedDescription)")
+        }
     }
     @IBAction func cancelButton(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension detalleViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage{
+            imagenPerson.image = selectedImage
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func cargarCoreData(){
+        let fetchRequest: NSFetchRequest <Contacto> = Contacto.fetchRequest()
+        
+        do {
+            contactos = try contexto.fetch(fetchRequest)
+        } catch  {
+            print("error al cargar contactos del coreData: \(error.localizedDescription)")
+        }
+    }
 }
